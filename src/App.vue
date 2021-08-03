18011860b4faa4edd82493ee2d4c3b34c1fe7784
Maxime Moreillon <m.moreillon@gmail.com>
2021-08-03 12:22:15 +0000

    greetings

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ export default {
     options: {
       title: "IoT Dashboard",
       authenticate: true,
-      skip_greetings: true,
+      skip_greetings: process.env.NODE_ENV === 'development',
       login_url: `${process.env.VUE_APP_USER_MANAGER_API_URL}/auth/login`,
       identification_url: `${process.env.VUE_APP_USER_MANAGER_API_URL}/users/self`,
     },
