4bc2e85221d166c1790d2013c57df7b6ecdfba88
Maxime Moreillon <m.moreillon@gmail.com>
2023-05-04 09:33:16 +0900

    minor

--- src/components/Device.vue
+++ src/components/Device.vue
@@ -1,40 +1,26 @@
 <template>
-
-  <v-card
-    outlined>
-
+  <v-card outlined>
     <v-toolbar flat>
-      <v-toolbar-title>{{device.nickname || device.name}}</v-toolbar-title>
-      <v-spacer></v-spacer>
-
+      <v-toolbar-title>{{ device.nickname || device.name }}</v-toolbar-title>
+      <v-spacer />
 
       <v-btn
         v-if="device.connected && device.state"
         icon
         :disabled="!device.connected"
         @click="toggle()"
-        :color="device.state === 'on' ? '#c00000' : ''">
+        :color="device.state === 'on' ? '#c00000' : ''"
+      >
         <v-icon>mdi-power</v-icon>
       </v-btn>
 
-      <v-btn
-        v-if="!device.connected"
-        icon
-        disabled>
+      <v-btn v-if="!device.connected" icon disabled>
         <v-icon>mdi-wifi-off</v-icon>
       </v-btn>
 
-
-
-
-
-
       <v-menu offset-y>
         <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on">
+          <v-btn icon v-bind="attrs" v-on="on">
             <v-icon>mdi-dots-vertical</v-icon>
           </v-btn>
         </template>
@@ -42,9 +28,7 @@
         <v-list dense>
           <v-list-item>
             <v-list-item-content>
-              <v-btn
-                text
-                @click="clear_retained_messages()">
+              <v-btn text @click="clear_retained_messages()">
                 <v-icon>mdi-delete</v-icon>
                 <span>Remove</span>
               </v-btn>
@@ -52,26 +36,25 @@
           </v-list-item>
         </v-list>
       </v-menu>
-
     </v-toolbar>
 
     <v-expansion-panels flat>
       <v-expansion-panel>
-        <v-expansion-panel-header>
-          Device details
-        </v-expansion-panel-header>
+        <v-expansion-panel-header> Device details </v-expansion-panel-header>
         <v-expansion-panel-content>
           <v-list>
             <v-list-item two-line>
               <v-list-item-content>
                 <v-list-item-subtitle>Name</v-list-item-subtitle>
-                <v-list-item-title>{{device.name}}</v-list-item-title>
+                <v-list-item-title>{{ device.name }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
             <v-list-item two-line>
               <v-list-item-content>
                 <v-list-item-subtitle>Type</v-list-item-subtitle>
-                <v-list-item-title>{{device.type || 'unknown'}}</v-list-item-title>
+                <v-list-item-title>
+                  {{ device.type || "unknown" }}
+                </v-list-item-title>
               </v-list-item-content>
             </v-list-item>
             <v-list-item two-line>
@@ -80,8 +63,10 @@
                 <v-list-item-title>
                   <a
                     v-if="this.device.address"
-                    :href="`http://${this.device.address}`">
-                    http://{{this.device.address}}
+                    :href="`http://${this.device.address}`"
+                    target="_blank"
+                  >
+                    http://{{ this.device.address }}
                   </a>
                   <span v-else>Unknown</span>
                 </v-list-item-title>
@@ -90,25 +75,25 @@
             <v-list-item two-line>
               <v-list-item-content>
                 <v-list-item-subtitle>Status topic</v-list-item-subtitle>
-                <v-list-item-title>{{device.status_topic}}</v-list-item-title>
+                <v-list-item-title>{{ device.status_topic }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
             <v-list-item two-line>
               <v-list-item-content>
                 <v-list-item-subtitle>Command topic</v-list-item-subtitle>
-                <v-list-item-title>{{command_topic}}</v-list-item-title>
+                <v-list-item-title>{{ command_topic }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
             <v-list-item two-line>
               <v-list-item-content>
                 <v-list-item-subtitle>Connected</v-list-item-subtitle>
-                <v-list-item-title>{{device.connected}}</v-list-item-title>
+                <v-list-item-title>{{ device.connected }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
             <v-list-item two-line>
               <v-list-item-content>
                 <v-list-item-subtitle>State</v-list-item-subtitle>
-                <v-list-item-title>{{device.state}}</v-list-item-title>
+                <v-list-item-title>{{ device.state }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
 
@@ -116,103 +101,82 @@
               <v-list-item-content>
                 <v-list-item-subtitle>Firmware version</v-list-item-subtitle>
                 <v-list-item-title>
-                  {{
-                    device.firmware_version
-                    || device.version
-                    || 'unknown'
-                  }}
+                  {{ device.firmware_version || device.version || "unknown" }}
                 </v-list-item-title>
               </v-list-item-content>
             </v-list-item>
 
-            <v-list-item
-              two-line
-              v-if="device.type === 'ircontroller'">
+            <!-- Special addition for IR controllers -->
+            <v-list-item two-line v-if="device.type === 'ircontroller'">
               <v-list-item-content>
                 <v-list-item-subtitle>IR signal recording</v-list-item-subtitle>
                 <v-row align="center">
                   <v-col cols="auto">
-                    <v-btn
-                      @click="record('on')">
-                      Record ON
-                    </v-btn>
+                    <v-btn @click="record('on')"> Record ON </v-btn>
                   </v-col>
                   <v-col cols="auto">
-                    <v-btn
-                      @click="record('off')">
-                      Record OFF
-                    </v-btn>
+                    <v-btn @click="record('off')"> Record OFF </v-btn>
                   </v-col>
-                  <v-spacer/>
+                  <v-spacer />
                 </v-row>
-
-
-
-
               </v-list-item-content>
             </v-list-item>
           </v-list>
-
-
-
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
-
-
   </v-card>
-
-
 </template>
 
 <script>
-import mqtt_client from '@/mqtt_client.js'
+import mqtt_client from "@/mqtt_client.js"
 
 export default {
-  name: 'Home',
+  name: "Device",
   props: {
-    device: Object
+    device: Object,
   },
-  components: {
-  },
-  data(){
-    return {
-    }
-  },
-  mounted(){
-  },
+
+  mounted() {},
   methods: {
-    turn_on(){
-      if(this.device.json) mqtt_client.publish(this.command_topic, JSON.stringify({state: 'on'}))
-      else mqtt_client.publish(this.command_topic, 'on')
+    turn_on() {
+      if (this.device.json)
+        mqtt_client.publish(this.command_topic, JSON.stringify({ state: "on" }))
+      else mqtt_client.publish(this.command_topic, "on")
     },
-    turn_off(){
-      if(this.device.json) mqtt_client.publish(this.command_topic, JSON.stringify({state: 'off'}))
-      else mqtt_client.publish(this.command_topic, 'off')
+    turn_off() {
+      if (this.device.json)
+        mqtt_client.publish(
+          this.command_topic,
+          JSON.stringify({ state: "off" })
+        )
+      else mqtt_client.publish(this.command_topic, "off")
     },
-    record(signal){
-      mqtt_client.publish(this.command_topic, JSON.stringify({record: signal}))
+    record(signal) {
+      mqtt_client.publish(
+        this.command_topic,
+        JSON.stringify({ record: signal })
+      )
     },
-    toggle(){
-      if(this.device.state === 'on') this.turn_off()
+    toggle() {
+      if (this.device.state === "on") this.turn_off()
       else this.turn_on()
     },
-    clear_retained_messages(){
-      if(!confirm(`Remove device ${this.device.name}?`)) return
+    clear_retained_messages() {
+      if (!confirm(`Remove device ${this.device.name}?`)) return
       const options = { retain: true }
       mqtt_client.publish(this.device.status_topic, new ArrayBuffer(), options)
-      this.$store.commit('remove_device', this.device)
-    }
+      this.$store.commit("remove_device", this.device)
+    },
   },
   computed: {
-    username(){
+    username() {
       return this.$store.state.current_user.username
     },
-    command_topic(){
+    command_topic() {
       // not exactly elegant
-      return this.device.status_topic.replace('/status','/command')
+      return this.device.status_topic.replace("/status", "/command")
     },
-  }
-
+  },
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,74 +1,66 @@
 <template>
   <v-card max-width="60rem" class="mx-auto">
+    <v-card-title>{{ username }}'s devices</v-card-title>
 
-    <v-card-title>{{username}}'s devices</v-card-title>
+    <v-card-text>
+      <template v-if="!$store.state.mqtt_connected">
+        <v-row justify="center">
+          <v-col class="text-center">
+            <v-progress-circular indeterminate />
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col class="text-center">
+            <span>Connecting to MQTT</span>
+          </v-col>
+        </v-row>
+      </template>
 
-
-
-    <!-- $mqtt.connect not reactive enough it seems-->
-    <v-container
-      v-if="!$store.state.mqtt_connected"
-      class="mt-12">
-      <v-row justify="center">
-        <v-col class="text-center">
-          <v-progress-circular indeterminate/>
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-        <v-col class="text-center">
-          <span>Connecting to MQTT</span>
-        </v-col>
-      </v-row>
-    </v-container>
-
-    <v-container
-      v-else-if="$store.state.devices.length < 1"
-      class="mt-12">
-      <v-row justify="center">
-        <v-col class="text-center">
-          <v-progress-circular indeterminate/>
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-        <v-col class="text-center">
-          <span>Waiting for devices</span>
-        </v-col>
-      </v-row>
-    </v-container>
-
-    <v-card-text v-else>
-      <v-row v-for="(device, index) in $store.state.devices" :key="`device_${index}`" dense>
-        <v-col cols="12">
-          <Device   :device="device" />
-        </v-col>
-      </v-row>
+      <template v-else-if="$store.state.devices.length < 1">
+        <v-row justify="center">
+          <v-col class="text-center">
+            <v-progress-circular indeterminate />
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col class="text-center">
+            <span>Waiting for devices</span>
+          </v-col>
+        </v-row>
+      </template>
+      <template v-else>
+        <v-row
+          v-for="(device, index) in $store.state.devices"
+          :key="`device_${index}`"
+          dense
+        >
+          <v-col cols="12">
+            <Device :device="device" />
+          </v-col>
+        </v-row>
+      </template>
     </v-card-text>
-
-
   </v-card>
-
 </template>
 
 <script>
-  import Device from '@/components/Device.vue'
+import Device from "@/components/Device.vue"
 
-  export default {
-    name: 'Home',
-    components: {
-      Device
+export default {
+  name: "Home",
+  components: {
+    Device,
+  },
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+  computed: {
+    username() {
+      const current_user = this.$store.state.current_user
+      return current_user.display_name || current_user.username
     },
-    data(){
-      return {}
-    },
-    mounted(){},
-    methods: {},
-    computed: {
-      username(){
-        const current_user = this.$store.state.current_user
-        return current_user.display_name
-          || current_user.username
-      },
-    }
-
-  }
+  },
+}
 </script>
